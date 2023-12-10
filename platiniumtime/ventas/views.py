69a714154d1c5055c06e97b53b73c9e93c8c7f75
LUISROMERO2 <144833409+LUISROMERO2@users.noreply.github.com>
2023-12-10 17:02:47 -0500

    Delete platiniumtime/ventas/views.py

--- platiniumtime/ventas/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import productos
-
-def lista_productos(request):
-    lista_productos = productos.objects.all()
-    
-    return render(request, 'ventas.html', {'lista_productos': lista_productos})
